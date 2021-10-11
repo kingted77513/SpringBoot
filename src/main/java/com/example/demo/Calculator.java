5ec469688074aa01c6b21a0e2e0ee68bc6b662d1
kingted77513 <kingted77513@gmail.com>
2021-10-11 12:16:12 +0800

    新增divide方法與測試

--- src/main/java/com/example/demo/Calculator.java
+++ src/main/java/com/example/demo/Calculator.java
@@ -2,13 +2,17 @@ package com.example.demo;
 
 public class Calculator {
 
-    public int add (int x, int y){
+    public int add(final int x, final int y) {
         return x + y;
     }
 
-    public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        int result = calculator.add(1, 2);
+    public static void main(final String[] args) {
+        final Calculator calculator = new Calculator();
+        final int result = calculator.add(1, 2);
         System.out.println("result = " + result);
+    }
+
+    public int divide(final int x, final int y) {
+        return x / y;
     }
 }
--- src/test/java/com/example/demo/CalculatorTest.java
+++ src/test/java/com/example/demo/CalculatorTest.java
@@ -3,16 +3,26 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CalculatorTest {
 
     @Test
-    public void test() {
-        Calculator calculator = new Calculator();
-        int result = calculator.add(1, 2);
+    public void add() {
+        final Calculator calculator = new Calculator();
+        final int result = calculator.add(1, 2);
 
         assertEquals(3, result, "加法有問題");
+    }
+
+    @Test
+    public void divide() {
+        final Calculator calculator = new Calculator();
+        final int result = calculator.divide(10, 2);
+
+        assertEquals(5, result);
+        assertThrows(ArithmeticException.class, () -> calculator.divide(1, 0));
     }
 
 }
