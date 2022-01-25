2726b302eafa2648cc1ac079d8c4c7ed8af6b619
dev41 <YGYebT2b>
2022-01-25 08:19:12 +0800

    新增@JsonInclude可以濾掉非null的值

--- src/main/java/com/example/demo/objectmapper/User.java
+++ src/main/java/com/example/demo/objectmapper/User.java
@@ -1,5 +1,8 @@
 package com.example.demo.objectmapper;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class User {
     Integer id;
     String name;
