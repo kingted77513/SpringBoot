acb85e0d0fae22630e73826af6237ac4b7e3a1d6
dev41 <YGYebT2b>
2022-01-28 08:36:33 +0800

    新增 getForEntity 的案例

--- src/main/java/com/example/demo/resttemplate/RestTemplateController.java
+++ src/main/java/com/example/demo/resttemplate/RestTemplateController.java
@@ -2,6 +2,7 @@ package com.example.demo.resttemplate;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -34,5 +35,22 @@ public class RestTemplateController {
         Student student = restTemplate.getForObject(url, Student.class, queryParamMap);
 
         return "getForObject success with param";
+    }
+
+    @GetMapping("/getForEntity")
+    public String getForEntity(){
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://mocki.io/v1/3275feb1-25cc-4cc6-8d09-726866048e59";
+        ResponseEntity<Student> studentEntity = restTemplate.getForEntity(url, Student.class);
+
+        System.out.println("student entity status code: " + studentEntity.getStatusCode());
+
+        Student student = studentEntity.getBody();
+        System.out.println("student id: "  + student.getId());
+        System.out.println("student name: " + student.getName());
+        System.out.println("student phone: " + student.getContactPhone());
+
+        return "getForObject success";
     }
 }
