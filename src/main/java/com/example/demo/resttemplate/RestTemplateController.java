9fda8d8b98a03495e8264b141c9265974f0456be
dev41 <YGYebT2b>
2022-01-28 08:50:20 +0800

    新增 getForExchange 和 postForExchange 的案例

--- src/main/java/com/example/demo/resttemplate/RestTemplateController.java
+++ src/main/java/com/example/demo/resttemplate/RestTemplateController.java
@@ -2,6 +2,10 @@ package com.example.demo.resttemplate;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -87,6 +91,45 @@ public class RestTemplateController {
         System.out.println("student id: "  + student.getId());
         System.out.println("student name: " + student.getName());
         System.out.println("student phone: " + student.getContactPhone());
+
+        return "getForObject success";
+    }
+
+    @GetMapping("/getForExchange")
+    public String getForExchange(){
+
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.set("header1", "123");
+
+        HttpEntity requestEntity = new HttpEntity(requestHeaders);
+
+        Map<String, Object> queryParamMap = new HashMap<>();
+        queryParamMap.put("graduate", true);
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://mocki.io/v1/3275feb1-25cc-4cc6-8d09-726866048e59";
+        ResponseEntity<Student> studentEntity = restTemplate.exchange(
+            url, HttpMethod.GET, requestEntity, Student.class, queryParamMap);
+
+        return "getForObject success";
+    }
+
+    @GetMapping("/postForExchange")
+    public String postForExchange(){
+
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.set("header1", "123");
+        requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        Student requestBody = new Student();
+        requestBody.setName("John");
+
+        HttpEntity requestEntity = new HttpEntity(requestBody, requestHeaders);
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://mocki.io/v1/3275feb1-25cc-4cc6-8d09-726866048e59";
+        ResponseEntity<Student> studentEntity = restTemplate.exchange(
+            url, HttpMethod.POST, requestEntity, Student.class);
 
         return "getForObject success";
     }
