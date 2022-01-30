540279b1457106dbfbc622854175c17712a9caf9
dev41 <YGYebT2b>
2022-01-30 12:37:53 +0800

    新增 thymeleaf 跳轉案例

--- src/main/java/com/example/demo/controller/ThymeleafController.java
+++ src/main/java/com/example/demo/controller/ThymeleafController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.Student;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +11,20 @@ public class ThymeleafController {
     @GetMapping("/home")
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/my-student")
+    public String getStudent(Model model) {
+        Student student = new Student();
+        student.setId(1);
+        student.setName("Judy");
+
+        model.addAttribute("myStudent", student);
+        return "student";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
     }
 }
